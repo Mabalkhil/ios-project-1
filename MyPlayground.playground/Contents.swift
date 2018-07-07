6f3fbd557e2d123655e74081af241210164aed04
Mohammed Abalkhil <m.abalkhil@outlook.com>
2018-07-07 20:10:30 +0300

    adding playerr info and players array

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,139 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// player informations
+var player1 : [String:Any] = [
+    "name" : "Jill Tanner",
+    "height" : 36,
+    "soccerExperience" : true,
+    "guardian" : "Clara Tanner"
+]
+
+var player2 : [String:Any] = [
+    "name" : "Joe Smith",
+    "height" : 42,
+    "soccerExperience" : true,
+    "guardian" : "Jim and Jan Smith"
+]
+
+var player3 : [String:Any] = [
+    "name" : "Bill Bon",
+    "height" : 43,
+    "soccerExperience" : true,
+    "guardian" : "Sara and Jenny Bon"
+]
+
+var player4 : [String:Any] = [
+    "name" : "Eva Gordon",
+    "height" : 45,
+    "soccerExperience" : false,
+    "guardian" : "Wendy and Mike Gordon"
+]
+
+var player5 : [String:Any] = [
+    "name" : "Matt Gill",
+    "height" : 40,
+    "soccerExperience" : false,
+    "guardian" : "Charles and Sylvia Gill"
+]
+
+
+var player6 : [String:Any] = [
+    "name" : "Kimmy Stein",
+    "height" : 41,
+    "soccerExperience" : false,
+    "guardian" : "Bill and Hillary Stein"
+]
+
+var player7 : [String:Any] = [
+    "name" : "Sammy Adams",
+    "height" : 45,
+    "soccerExperience" : false,
+    "guardian" : "Jeff Adams"
+]
+
+var player8 : [String:Any] = [
+    "name" : "Karl Saygan",
+    "height" : 42,
+    "soccerExperience" : true,
+    "guardian" : "Heather Bledsoe"
+]
+
+var player9 : [String:Any] = [
+    "name" : "Suzane Greenberg",
+    "height" : 44,
+    "soccerExperience" : true,
+    "guardian" : "Henrietta Dumas"
+]
+
+var player10 : [String:Any] = [
+    "name" : "Sal Dali",
+    "height" : 41,
+    "soccerExperience" : false,
+    "guardian" : "Gala Dali"
+]
+
+var player11 : [String:Any] = [
+    "name" : "Joe Kavalier",
+    "height" : 39,
+    "soccerExperience" : false,
+    "guardian" : "Sam and Elaine Kavalier"
+]
+
+var player12 : [String:Any] = [
+    "name" : "Ben Finkelstein",
+    "height" : 44,
+    "soccerExperience" : false,
+    "guardian" : "Aaron and Jill Finkelstein"
+]
+
+var player13 : [String:Any] = [
+    "name" : "Diego Soto",
+    "height" : 41,
+    "soccerExperience" : true,
+    "guardian" : "Robin and Sarika Soto"
+]
+
+var player14 : [String:Any] = [
+    "name" : "Chloe Alaska",
+    "height" : 47,
+    "soccerExperience" : false,
+    "guardian" : "David and Jamie Alaska"
+]
+
+var player15 : [String:Any] = [
+    "name" : "Arnold Willis",
+    "height" : 43,
+    "soccerExperience" : false,
+    "guardian" : "Claire Willis"
+]
+
+
+var player16 : [String:Any] = [
+    "name" : "Phillip Helm",
+    "height" : 44,
+    "soccerExperience" : true,
+    "guardian" : "Thomas Helm and Eva Jones"
+]
+
+var player17 : [String:Any] = [
+    "name" : "Les Clay",
+    "height" : 42,
+    "soccerExperience" : true,
+    "guardian" : "Wynonna Brown"
+]
+
+var player18 : [String:Any] = [
+    "name" : "Herschel Krustofski",
+    "height" : 45,
+    "soccerExperience" : true,
+    "guardian" : "Hyman and Rachel Krustofski"
+]
+
+// players array hold all the information about the player
+var players = [player1,player2,player3,player4,player5,player6,player7,player8,player9,player10,player11,player12,player13,player14,player15]
+
+
+
+
